7ae69199d3a584a9c1f2635709882954ff278fc6
Ashmi Sinha <66667107+ashmisinha@users.noreply.github.com>
2023-10-02 11:06:29 +0530

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -5,7 +5,7 @@ const hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Hello From TWS Batch 4 - You all are awesome!",
+        message: "This is a Node HTTP API Project!",
       },
       null,
       2
